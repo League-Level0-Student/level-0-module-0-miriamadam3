7a48bbfbbdad997470afab87eb356063e3bcb5ad
root <root@c5f082ee5800>
2020-07-02 23:50:34 +0000

    7/2/20

--- src/_03_print_and_popups/_1_greeter/Greeter.java
+++ src/_03_print_and_popups/_1_greeter/Greeter.java
@@ -1,9 +1,13 @@
 package _03_print_and_popups._1_greeter;
 
+import javax.swing.JOptionPane;
+
 import _02_code_flow._3_pentagon_crazy.PentagonCrazy;
 
 public class Greeter {
 	public static void main(String[] args) {
 		System.out.println("hellooooo");
+		String input = JOptionPane.showInputDialog("What is your name?");
+		JOptionPane.showMessageDialog(null,"Hello "+input);
 	}
 }
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,11 +8,18 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
+		String code=JOptionPane.showInputDialog("Do you know how to write a code?");
 
 		// 2. If they say "yes", tell them they will rule the world.
-
+if (code. equals( "yes")) {
+	
+	JOptionPane.showMessageDialog(null, "You will rule the world!");
+	
+	
+}
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+else {
+JOptionPane.showMessageDialog(null, "Then sign up for classes at The League!");}
 	}
 }
 
