f5d37539cf653b6af35eb4e5762654cb55b7f22f
AmalM250 <57469472+AmalM250@users.noreply.github.com>
2020-07-09 16:59:33 -0700

    7/9/20 Quiz Game

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,15 +1,29 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
+		int baseScore =0;
 		
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+		String answer=JOptionPane.showInputDialog("What is 2+2?");
+
 				// 2.  Ask the user a question 
 				
 				// 3.  Use an if statement to check if their answer is correct
+			if(answer.equals("4")) {
+				System.out.println("true");
+				baseScore++;
+			}	
+				
+				answer=JOptionPane.showInputDialog(" What does red and blue make?");	
+			if(answer.equals ("purple")) {
+				System.out.println(true);
+				baseScore++;
+			}
 				
 				// 4.  if the user's answer was correct, add one to their score 
 		
@@ -18,6 +32,20 @@ public class QuizGame {
 		
 		
 		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null, "Final Score "+baseScore);
 	}
 }
+/*
+ * What is 2+2?
+ * 4
+ * 
+ * What does red and blue make?
+ * Purple
+ * 
+ * What color is the sky?
+ * Blue
+ * 
+ * What is 3+5?
+ * 8
+ 
+*/
